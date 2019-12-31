41b5883cc00d9921428146addcb82b5bf6e57584
Swadhin Saha <rahul4kuet2k10@gmail.com>
2019-12-31 15:00:10 +0600

    The Decorator Pattern

---
+++ The Decorator Pattern/index.php
@@ -0,0 +1,71 @@
+<?php
+
+
+interface CarService
+{
+    public function getCost();
+
+    public function getDescription();
+}
+
+class BasicInspection implements CarService
+{
+    public function getCost()
+    {
+        return 25;
+    }
+
+    public function getDescription()
+    {
+        return 'Basic Inspection';
+    }
+}
+
+
+class  OilChange implements CarService
+{
+    protected $carService;
+
+    public function __construct(CarService $carService)
+    {
+        $this->carService = $carService;
+    }
+
+    public function getCost()
+    {
+        return 29 + $this->carService->getCost();
+    }
+
+    public function getDescription()
+    {
+        return $this->carService->getDescription() . ', and Oil Change';
+    }
+
+}
+
+class TireRotation implements CarService
+{
+    protected $carService;
+
+    public function __construct(CarService $carService)
+    {
+        $this->carService = $carService;
+    }
+
+    public function getCost()
+    {
+        return 10 + $this->carService->getCost();
+    }
+
+    public function getDescription()
+    {
+        return $this->carService->getDescription() . ', and a Tire Rotation';
+    }
+}
+
+$service = new TireRotation(new OilChange(new BasicInspection()));
+
+echo $service->getDescription();
+echo "\n";
+echo $service->getCost();
+echo "\n";
